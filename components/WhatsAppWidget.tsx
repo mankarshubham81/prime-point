03513778760d1fb6e408db678507c99cb664a38e
Shubham Mankar <mankarshubham81@gmail.com>
2025-02-13 18:30:25 +0530

    tweaks mini

--- components/WhatsAppWidget.tsx
+++ components/WhatsAppWidget.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 const WhatsAppWidget = () => {
   return (
     <a
-      href="https://wa.me/8390671523"
+      href="https://wa.me/+918390671523"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-4 left-4 bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 z-50"
